c335fdaeee2d555a72879b7749c4f9d58afbad2e
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-22 14:02:35 +0300

    disable useUnifiedTopology

--- src/startup/database.js
+++ src/startup/database.js
@@ -1,11 +1,15 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const dbDebugger = require('debug')('app:db');
+const mongoose = require("mongoose");
+const config = require("config");
+const dbDebugger = require("debug")("app:db");
 
 module.exports = function() {
-// connect to mongodb
+  // connect to mongodb
   mongoose
-    .connect(config.get('db.url'), { useNewUrlParser: config.get('db.useNewUrlParser'), useFindAndModify: config.get('db.useFindAndModify') })
-    .then(() => dbDebugger('Database connected successfully.'))
-    .catch((O_O) => dbDebugger('Unable to connect to mongodb.', O_O));
+    .connect(config.get("db.url"), {
+      useNewUrlParser: config.get("db.useNewUrlParser"),
+      useFindAndModify: config.get("db.useFindAndModify"),
+      useUnifiedTopology: false
+    })
+    .then(() => dbDebugger("Database connected successfully."))
+    .catch(O_O => dbDebugger("Unable to connect to mongodb.", O_O));
 };
