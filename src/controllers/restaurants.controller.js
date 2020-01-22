a6c2472d2249203ebfad0a6d9b3932f61e2b5dcd
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-22 16:20:29 +0300

    finished show function

--- src/controllers/restaurants.controller.js
+++ src/controllers/restaurants.controller.js
@@ -5,7 +5,13 @@ async function index(request, response) {
   response.send(restaurants);
 }
 
-function show(request, response) {}
+async function show(request, response) {
+  const { id } = request.params;
+  const restaurant = await Restaurant.findById(id);
+
+  if (restaurant) response.send(restaurant);
+  else response.status(404).send({ message: "Restaurant Not Found" });
+}
 
 async function create(request, response) {
   const { name, logoPath } = request.body;
