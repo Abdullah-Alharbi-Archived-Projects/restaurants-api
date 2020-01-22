cbe7551f1946e7f26afffecf5e2f662172694ffc
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-22 16:03:09 +0300

    prevent favicon.ico request

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -2,4 +2,8 @@ const api = require("../routes/index");
 
 module.exports = function(app) {
   app.use("/api/", api);
+
+  // prevent GET /favicon.ico
+  // send 204 No Content
+  app.get("/favicon.ico", (request, response) => response.status(204));
 };
