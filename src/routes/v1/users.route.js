82a0d5493d3927dcc7fc194cee8b1393e68fec32
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-22 17:38:33 +0300

    users resource

--- src/routes/v1/users.route.js
+++ src/routes/v1/users.route.js
@@ -3,6 +3,23 @@ const { Router } = require("express");
 const userController = require("../../controllers/user.controller");
 const router = Router();
 
+// -------- [ADMIN] --------
+// GET /api/v1/users/ -> show all users [ADMIN]
 router.get("/", userController.index);
+
+// DELETE /api/v1/users/:id/ -> delete user [ADMIN]
+router.delete("/:id/", userController.destroy);
+
+// GET /api/v1/users/:id/ -> show one user [ADMIN, Authenticated]
+router.get("/:id/", userController.show);
+
+// PUT /api/v1/users/:id/ -> update user [ADMIN, Authenticated]
+router.put("/:id/", userController.update);
+
+// -------- [Guest] --------
+
+// POST /api/v1/users/ -> register new user [Guest]
+// TODO: need custom middleware to register token
+router.post("/", userController.create);
 
 module.exports = router;
