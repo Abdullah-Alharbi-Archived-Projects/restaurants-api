a3e53af45f84a6958b45f31a884b64936659531b
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-22 17:41:09 +0300

    finished index function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,9 @@
 const User = require("../models/User");
 
-async function index(request, response) {}
+async function index(request, response) {
+  const users = await User.find();
+  response.send(users);
+}
 
 async function show(request, response) {}
 
