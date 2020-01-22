8b9297a2fef510759759c8a4e2a2e23a3f304e0c
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-22 18:03:30 +0300

    finished create function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,16 @@ async function index(request, response) {
 
 async function show(request, response) {}
 
-async function create(request, response) {}
+async function create(request, response) {
+  const { firstName, lastName, email, password } = request.body;
+  let user = new User({ firstName, lastName, email, password });
+
+  await user.encrypt(password);
+  user = await user.save();
+  user = user.toJSON();
+
+  response.status(201).send({ message: "Created", user });
+}
 
 async function update(request, response) {}
 
