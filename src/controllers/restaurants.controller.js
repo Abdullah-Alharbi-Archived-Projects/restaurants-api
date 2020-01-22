6cc9e215c6ba268c2133c453b4b5a7cc5b50752c
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-22 14:39:41 +0300

    restaurants controller setup

---
+++ src/controllers/restaurants.controller.js
@@ -0,0 +1,19 @@
+const Restaurant = require("../models/Restaurant");
+
+function index(request, response) {}
+
+function show(request, response) {}
+
+function create(request, response) {}
+
+function update(request, response) {}
+
+function destroy(request, response) {}
+
+module.exports = {
+  index,
+  show,
+  create,
+  update,
+  destroy
+};
