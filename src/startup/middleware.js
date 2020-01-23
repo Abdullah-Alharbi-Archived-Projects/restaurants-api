ac238f6dc114ea3f4440d6058ef7823506a0dfe5
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-23 15:00:18 +0300

    initialize passport

--- src/startup/middleware.js
+++ src/startup/middleware.js
@@ -1,11 +1,13 @@
 const { json } = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
+const passport = require("passport");
 
 module.exports = function(app) {
   app.use(json());
   app.use(helmet());
   app.use(compression()); // compress all responses
 
+  app.use(passport.initialize());
   // TODO: need global catch middleware
 };
