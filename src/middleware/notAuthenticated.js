538e5a1b0a17231ddfcddaa4c68f363d96392651
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-24 21:19:25 +0300

    updated because the jwt might not be in redis

--- src/middleware/notAuthenticated.js
+++ src/middleware/notAuthenticated.js
@@ -1,9 +1,16 @@
 const extractJwt = require("../services/extractJwt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-module.exports = function(request, response, next) {
+module.exports = async function(request, response, next) {
+  const redis = request.app.get("redis");
   const token = extractJwt(request);
 
-  if (token) {
+  const { id } = jwt.verify(token, config.get("jwtSecretKey"));
+
+  const result = await redis.validate(id, token);
+
+  if (result) {
     return response.status(403).send({ message: "Forbidden" });
   }
 
