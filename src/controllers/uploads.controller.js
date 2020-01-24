e2f51a21446efbd5a60cecb0655cbf0b85fe90e6
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-24 20:21:05 +0300

    finished upload function

--- src/controllers/uploads.controller.js
+++ src/controllers/uploads.controller.js
@@ -1,4 +1,17 @@
-function upload(request, response) {}
+const uploadService = require("../services/imageUpload");
+
+function upload(request, response) {
+  if (!request.files)
+    return response.status(400).send({ message: "No Files Selected." });
+
+  const result = uploadService(request.files);
+  if (result)
+    return response.status(201).send({ message: "Uploaded Successfully." });
+
+  return response
+    .status(500)
+    .send({ message: "Unable to Upload. Internal Server Error." });
+}
 
 module.exports = {
   upload
