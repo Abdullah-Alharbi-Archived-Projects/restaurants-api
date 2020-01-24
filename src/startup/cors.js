887ac3be86bd44e80541bba224b29b8eff902447
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-24 20:23:16 +0300

    removed white list

--- src/startup/cors.js
+++ src/startup/cors.js
@@ -1,17 +1,7 @@
 const cors = require("cors");
 
 module.exports = function(app) {
-  const whiteList = ["http://localhost"];
+  // TODO: add white list
 
-  const corsOptions = {
-    origin: function(origin, callback) {
-      if (whiteList.indexOf(origin) !== -1) {
-        return callback(null, true);
-      }
-
-      callback(new Error("Not Allowed By CORS."));
-    }
-  };
-
-  app.use(cors(corsOptions));
+  app.use(cors());
 };
