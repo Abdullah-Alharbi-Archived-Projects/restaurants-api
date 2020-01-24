d657f096e72f3a3dfc17591cde1acecf7469b5fd
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-24 20:20:25 +0300

    removed prmoise

--- src/services/imageUpload.js
+++ src/services/imageUpload.js
@@ -1,16 +1,18 @@
 const _ = require("lodash");
 const uuid = require("uuid/v1");
+const config = require("config");
 
-module.exports = function(files, path = "/uploads") {
-  return new Promise((resolve, reject) => {
-    try {
-      _.forEach(_.keysIn(files), key => {
-        const file = files[key];
-        file.mv(`${path}/${uuid()}.${file.mimetype}`);
-      });
-      resolve(true);
-    } catch (error) {
-      reject(error);
-    }
-  });
+module.exports = function(files, folder = "uploads") {
+  try {
+    _.forEach(_.keysIn(files), key => {
+      const file = files[key];
+      const extention = file.name.match(/\.\w+/)[0];
+      file.mv(`./src/${config.get("static")}/${folder}/${uuid() + extention}`);
+    });
+
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 };
