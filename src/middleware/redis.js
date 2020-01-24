54903f3cf2313d4c3456d3b60b4a390f11f6dcc1
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-24 21:18:32 +0300

    updated

--- src/middleware/redis.js
+++ src/middleware/redis.js
@@ -1,13 +1,12 @@
 const extractJwt = require("../services/extractJwt");
 
 module.exports = async function(request, response, next) {
-  const user = request.user;
   const redis = request.app.get("redis");
-
-  const reply = await redis.get(user._id);
-
+  const user = request.user;
   const token = extractJwt(request);
-  if (reply === token) {
+
+  const result = await redis.validate(user._id, token);
+  if (result) {
     request.redis = redis;
     return next();
   }
