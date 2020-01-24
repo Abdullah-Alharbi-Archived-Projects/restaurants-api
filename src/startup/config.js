44ef190f42c761014347f8f0d989987e79b15d6f
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-24 20:43:13 +0300

    fixed static path [middleware]

--- src/startup/config.js
+++ src/startup/config.js
@@ -1,19 +1,23 @@
 const { static } = require("express");
+const { join } = require("path");
 const config = require("config");
 const serverDebugger = require("debug")("app:server");
 
 module.exports = function(app) {
   if (config.has("static") && config.get("static")) {
-    app.use(static(config.get("static")));
+    const path = join(__dirname, "..", config.get("static"));
+    app.use(static(path));
     serverDebugger(`Serve static files is active "${config.get("static")}".`);
   }
 
   // note: if you are developing api disable that
   if (config.has("etag") && !config.get("etag")) {
     app.disable("etag");
+    serverDebugger("Etag is disabled.");
   }
 
   if (config.has("powered-by") && config.get("powered-by")) {
     app.disable("x-powered-by");
+    serverDebugger("-powered-by is disabled.");
   }
 };
