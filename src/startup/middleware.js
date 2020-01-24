96d35ae2a9c9b08e2bb9b253880848e6c0418d77
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-24 15:24:54 +0300

    added passport-jwt strategy

--- src/startup/middleware.js
+++ src/startup/middleware.js
@@ -2,12 +2,34 @@ const { json } = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
 const passport = require("passport");
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const config = require("config");
+const { User } = require("../models/User");
 
 module.exports = function(app) {
   app.use(json());
   app.use(helmet());
   app.use(compression()); // compress all responses
 
+  // passport setup
   app.use(passport.initialize());
+
+  const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.get("jwtSecretKey")
+  };
+
+  const strategy = new JwtStrategy(options, async ({ id }, done) => {
+    const user = await User.findById(id);
+    // TODO: need to validate the expire & add refresh token
+
+    if (user) {
+      return done(null, user.toJSON());
+    }
+
+    return done(null, false);
+  });
+  passport.use(strategy);
+
   // TODO: need global catch middleware
 };
