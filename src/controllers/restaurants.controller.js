dd5141cc0c7ecfdac2b29efac94c43240faeaf27
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-26 09:23:47 +0300

    restaurants handling uploads

--- src/controllers/restaurants.controller.js
+++ src/controllers/restaurants.controller.js
@@ -1,7 +1,11 @@
 const Restaurant = require("../models/Restaurant");
+const uploadService = require("../services/imageUpload");
+const { User } = require("../models/User");
+const { Image } = require("../models/Image");
+const _ = require("lodash");
 
 async function index(request, response) {
-  const restaurants = await Restaurant.find();
+  const restaurants = await Restaurant.find().populate("user");
   response.send(restaurants);
 }
 
@@ -15,10 +19,30 @@ async function show(request, response) {
 }
 
 async function create(request, response) {
-  const { name, logoPath } = request.body;
+  const { name, address } = request.body;
   // TODO: validate data
-  let restaurant = new Restaurant({ name, logoPath });
-  restaurant = await restaurant.save();
+
+  let restaurant = new Restaurant({ name, address });
+  if (request.files) {
+    const { logoPath } = request.files;
+    let [result, paths] = uploadService([logoPath]);
+    if (result) restaurant.logoPath = new Image({ path: paths[0] });
+
+    const files = _.omit(request.files, ["logoPath"]);
+    [result, paths] = uploadService(files);
+
+    if (result) {
+      const user = await User.findById(request.user._id);
+      paths.forEach(path => {
+        const image = new Image({ path });
+        user.images.push(image);
+        restaurant.images.push(image);
+      });
+      await user.save();
+    }
+  }
+  restaurant.user = request.user._id;
+  restaurant = (await restaurant.save()).populate("user");
   response.status(201).send({ message: "Created", restaurant });
 }
 
