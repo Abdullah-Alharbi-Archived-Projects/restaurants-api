69761c09d1e3bb55650b4dee145f8b6f2d84c916
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-27 16:06:46 +0300

    added new version:

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,14 @@
 const { Router } = require("express");
+
 const v1 = require("./v1/");
+const v2 = require("./v2/");
 
 const router = Router();
 
 // versions goes here
 router.use("/v1/", v1);
+
+// google map api
+router.use("/v2/", v2);
 
 module.exports = router;
