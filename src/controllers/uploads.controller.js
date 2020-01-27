f96ffef22e62800aad4fc08ed9e6cf024862ac18
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-27 12:31:47 +0300

    finished upload & delete images for restaurants model

--- src/controllers/uploads.controller.js
+++ src/controllers/uploads.controller.js
@@ -1,27 +1,86 @@
 const uploadService = require("../services/imageUpload");
-const { User } = require("../models/User");
+const deleteService = require("../services/imageDelete");
 const { Image } = require("../models/Image");
+const Restaurant = require("../models/Restaurant");
 
 async function upload(request, response) {
   if (!request.files)
     return response.status(400).send({ message: "No Files Selected." });
 
-  const [result, paths] = uploadService(request.files);
-  if (result) {
-    const user = await User.findById(request.user._id);
-    paths.forEach(path => {
-      const image = new Image({ path });
-      user.images.push(image);
-    });
-    await user.save();
-    return response.status(201).send({ message: "Uploaded Successfully." });
-  }
+  const { resource, target } = request.params;
 
-  return response
-    .status(500)
-    .send({ message: "Unable to Upload. Internal Server Error." });
+  let images = request.files["images[]"];
+
+  if (!images)
+    return response.status(400).send({ message: "No Files Selected." });
+
+  if (typeof images === "object") images = [images];
+
+  switch (resource) {
+    case "restaurants":
+      const restaurant = await Restaurant.findById(target);
+
+      if (restaurant) {
+        const [result, paths] = uploadService(images);
+        if (result) {
+          paths.forEach(path => {
+            const image = new Image(path);
+            restaurant.images.push(image);
+          });
+
+          const updatedRestaurant = await restaurant.save();
+          return response.send({
+            message: "Uploaded Successfully",
+            updatedRestaurant
+          });
+        }
+
+        return response.status(400).send({ message: "Bad Request." });
+      }
+
+      return response.status(404).send({ message: `Restaurant Not Found.` });
+
+    default:
+      return response
+        .status(404)
+        .send({ message: `Resource: ${resource} Not Found.` });
+  }
+}
+
+async function destroy(request, response) {
+  const { resource, target } = request.params;
+
+  const { items } = request.body;
+
+  if (!items)
+    return response.status(400).send({ message: "items is required." });
+
+  switch (resource) {
+    case "restaurants":
+      const restaurant = await Restaurant.findById(target);
+
+      if (restaurant) {
+        items.forEach(async item => {
+          const image = await restaurant.images.id(item);
+
+          if (image) {
+            await deleteService(await image.remove());
+          }
+        });
+        await restaurant.save();
+        return response.send({ message: "Images Deleted!" });
+      }
+
+      return response.status(404).send({ message: "Restaurant Not Found." });
+
+    default:
+      return response
+        .status(404)
+        .send({ message: `Resource: ${resource} Not Found.` });
+  }
 }
 
 module.exports = {
-  upload
+  upload,
+  destroy
 };
