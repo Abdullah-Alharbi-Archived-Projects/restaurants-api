392f7b60a00ae759cf782147ebf8aed8c0975a06
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-27 12:40:12 +0300

    setup comments controller

---
+++ src/controllers/comments.controller.js
@@ -0,0 +1,16 @@
+const { Comment } = require("../models/Comment");
+
+function index(request, response) {}
+
+function create(request, response) {}
+
+function update(request, response) {}
+
+function destroy(request, response) {}
+
+module.exports = {
+  index,
+  create,
+  update,
+  destroy
+};
